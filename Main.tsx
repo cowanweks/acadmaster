34aaadbccec0b42b3ad6d27d79f27f3b5428c8bf
cowanweks <cowannwekesa@gmail.com>
2023-10-22 14:15:05 +0300

    Updated app

--- Main.tsx
+++ Main.tsx
@@ -1,4 +1,4 @@
-import "./css/Index.css";
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "@containers/App";
--- containers/index.ts
+++ containers/index.ts
@@ -1,2 +1,11 @@
-export * from "./App"
-export * from "./Splash"
+import { Account } from "@containers/Account";
+import { App } from "@containers/App";
+import { Home } from "@containers/Home";
+import { Settings } from "@containers/Settings";
+import { Signin } from "@containers/Signin";
+import { Splash } from "@containers/Splash";
+import { Student } from "@containers/Student";
+import { Teacher } from "@containers/Teacher";
+
+
+export { Account, App, Home, Settings, Signin, Splash,Student,Teacher}
